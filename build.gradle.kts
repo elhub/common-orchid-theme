d2b1ffce3c4d17c092708476e322bf11f0c616cb
Sergii Prodan <serpro@disroot.org>
2021-06-22 14:02:31 +0200

    Add orchid and junit dependencies
    Test Plan:
    `./gradlew clean test`
    
    //Depends on D2349//
    
    Reviewers: O5 elhub/devtools, oscar.carlstrom
    
    Reviewed By: O5 elhub/devtools, oscar.carlstrom
    
    JIRA Issues: TD-1440
    
    Differential Revision: https://phabricator.elhub.cloud/D2352

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,11 @@ description = "Elhub Theme for Orchid Docs"
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.github.javaeden.orchid:OrchidCore:0.21.1")
+    testImplementation("io.github.javaeden.orchid:OrchidTest:0.21.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+}
+
+val test by tasks.getting(Test::class) {
+    useJUnitPlatform { }
 }
